12ec6c173226f7a48586e84445fd5ad39d4bb00a
VarHug <490641570@qq.com>
2019-04-09 20:53:38 +0800

    初始化项目

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Index from 'views/index.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/index'
+    },
+    {
+      path: '/index',
+      component: Index
+    }
+  ]
+})
